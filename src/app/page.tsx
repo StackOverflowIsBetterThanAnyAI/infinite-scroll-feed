e6aa45ffab43041a299808e2e9ba0bdb3d6d4e24
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-07 20:39:34 +0200

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Footer from '@/components/footer/Footer'
+import Navigation from '@/components/navigation/Navigation'
+
 export default function Home() {
-    return <div className="">Hello World!</div>
+    return (
+        <div className="min-h-svh flex flex-col items-center justify-start bg-gradient-to-b from-stone-700 to-stone-800">
+            <Navigation />
+            <Footer />
+        </div>
+    )
 }
