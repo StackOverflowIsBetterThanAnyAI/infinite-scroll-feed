ab3623d4052287c03fc918273f7c656d62655142
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-09 17:23:59 +0200

    focus trap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import Feed from '@/components/feed/Feed'
 import Footer from '@/components/footer/Footer'
 import Navigation from '@/components/navigation/Navigation'
+import { useFocusTrap } from '@/hooks/useFocusTrap'
 
 export default function Home() {
+    useFocusTrap()
+
     return (
         <div className="min-h-svh flex flex-col items-center justify-start bg-gradient-to-b from-stone-700 to-stone-800">
             <Navigation />
