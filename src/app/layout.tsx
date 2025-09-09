11d8d4a0444dde61cf0caf94ac6abbf0baa83792
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-09 12:11:20 +0200

    set backgroundcolor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" style={{ backgroundColor: '#44403b' }}>
             <body>{children}</body>
         </html>
     )
