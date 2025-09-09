07f00e24617eeb9347f4d00972fe84afc5d13564
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-09 11:39:46 +0200

    implement feed, placeholder for future implementations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Feed from '@/components/feed/Feed'
 import Footer from '@/components/footer/Footer'
 import Navigation from '@/components/navigation/Navigation'
 
@@ -5,6 +6,9 @@ export default function Home() {
     return (
         <div className="min-h-svh flex flex-col items-center justify-start bg-gradient-to-b from-stone-700 to-stone-800">
             <Navigation />
+            <p>Hero Section</p>
+            <p>Carousel with Top 10 Posts</p>
+            <Feed />
             <Footer />
         </div>
     )
