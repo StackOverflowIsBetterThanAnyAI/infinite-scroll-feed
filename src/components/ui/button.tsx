9a28d7759c9473a77abd26cd53cbe0c92980bce7
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-11 18:28:57 +0200

    adjust link-variant colors, adjust padding and margin for default size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,10 +18,10 @@ const buttonVariants = cva(
                 secondary:
                     'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
                 ghost: 'hover:bg-stone-700 active:bg-stone-600',
-                link: 'text-primary underline-offset-4 hover:underline',
+                link: 'text-primary hover:bg-stone-200/40 active:bg-stone-300/70',
             },
             size: {
-                default: 'h-9 px-2 py-1 mx-2',
+                default: 'h-9 p-1 mx-1.5',
                 sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
                 lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
                 icon: 'size-9',
