4e06f76f6e8754928955962291784a833f7056af
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-12 09:45:06 +0200

    users type

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,27 @@ export type FeedItemsType = {
     email: string
     body: string
 }
+
+export type UsersType = {
+    id: number
+    name: string
+    username: string
+    email: string
+    address: {
+        street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: string
+            lng: string
+        }
+    }
+    phone: string
+    website: string
+    company: {
+        name: string
+        catchPhrase: string
+        bs: string
+    }
+}
