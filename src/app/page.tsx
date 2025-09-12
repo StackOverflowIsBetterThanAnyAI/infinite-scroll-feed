03ed73bac52d324bb857c45e8cbe98b003c51ffb
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-09-12 13:38:31 +0200

    top users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,15 @@ import Feed from '@/components/feed/Feed'
 import Footer from '@/components/footer/Footer'
 import Hero from '@/components/hero/Hero'
 import Navigation from '@/components/navigation/Navigation'
+import Users from '@/components/users/Users'
 import { ContextTopTenPosts } from '@/context/ContextTopTenPosts'
+import { ContextTopUsers } from '@/context/ContextTopUsers'
 import { useFocusTrap } from '@/hooks/useFocusTrap'
 import { useSetWindowScrollY } from '@/hooks/useSetWindowScrollY'
 
 export default function Home() {
     const topTenPostsRef = useRef<HTMLDivElement | null>(null)
+    const topUsersRef = useRef<HTMLDivElement | null>(null)
 
     useFocusTrap()
     useSetWindowScrollY()
@@ -18,10 +21,13 @@ export default function Home() {
     return (
         <div className="min-h-svh flex flex-col items-center justify-start bg-gradient-to-b from-stone-700 to-stone-800">
             <ContextTopTenPosts.Provider value={topTenPostsRef}>
-                <Navigation />
-                <Hero />
-                <Feed />
-                <Footer />
+                <ContextTopUsers.Provider value={topUsersRef}>
+                    <Navigation />
+                    <Hero />
+                    <Users />
+                    <Feed />
+                    <Footer />
+                </ContextTopUsers.Provider>
             </ContextTopTenPosts.Provider>
         </div>
     )
